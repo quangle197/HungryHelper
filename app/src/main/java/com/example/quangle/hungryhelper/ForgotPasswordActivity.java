13bdb53b416d470b970c97654db8eb7c57ccff26
Sean <seandinhs@gmail.com>
2018-05-06 20:53:39 -0700

    Added more Java Classes

--- app/src/main/java/com/example/quangle/hungryhelper/ForgotPasswordActivity.java
+++ app/src/main/java/com/example/quangle/hungryhelper/ForgotPasswordActivity.java
@@ -1,7 +1,9 @@
 package com.example.quangle.hungryhelper;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 
 /**
  * Created by QuangLe on 5/6/2018.
@@ -15,5 +17,10 @@ public class ForgotPasswordActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.forgot_password);
 
+    }
+
+    public void gotoLogin(View v)
+    {
+        startActivity(new Intent(this, LoginActivity.class));
     }
 }
--- app/src/main/java/com/example/quangle/hungryhelper/LoginActivity.java
+++ app/src/main/java/com/example/quangle/hungryhelper/LoginActivity.java
@@ -4,18 +4,26 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.EditText;
 
 public class LoginActivity extends AppCompatActivity{
+    EditText usernameText;
+    EditText passwordText;
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.login_page);
-
+        usernameText = findViewById(R.id.loginPage_Username);
+        passwordText = findViewById(R.id.loginPage_Password);
     }
 
     public void gotoForgot(View v)
     {
         startActivity(new Intent(this, ForgotPasswordActivity.class));
+    }
+    public void gotoSignUp(View v)
+    {
+        startActivity(new Intent(this, SignupActivity.class));
     }
 }
